277f9bb285536a8d18cd0557a9fe04033ce514be
Zenirr <zenirr@mail.ru>
2022-08-22 23:45:00 +0600

    added and solved task_number_6

---
+++ task_number_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_number_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число = ");
+            int a =int.Parse(Console.ReadLine());
+            
+            if ((a%2) ==0)
+            Console.WriteLine($"Число {a} чётное");
+            else 
+            Console.WriteLine($"Число {a} нечётное");
+        }
+    }
+}
