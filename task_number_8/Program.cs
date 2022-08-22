0200cd556eab8bd554b2cef0ccc94a87dfb67245
Zenirr <zenirr@mail.ru>
2022-08-23 01:07:47 +0600

    added and solved task_number_8

---
+++ task_number_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_number_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число = ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            string numbers = "";
+
+            for (int i = 1; i<N;i++)
+            {
+                if ((i%2)==0)
+                numbers= numbers+$" {i}";
+            }
+            Console.WriteLine($"Чётные числа до {N} = {numbers}");
+        }
+    }
+}
