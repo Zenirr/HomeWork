44e483d85fe17faac4d61bf650d8fb2ce3206ec8
Zenirr <zenirr@mail.ru>
2022-08-22 22:38:47 +0600

    initial commit

---
+++ task_number_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task_number_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
