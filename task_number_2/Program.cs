1b9cc484c751917513c91554e495d2e554fe73f0
Zenirr <zenirr@mail.ru>
2022-08-22 22:59:08 +0600

    renamed task_number_1 into task_number_2 and solved first task

---
+++ task_number_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_number_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число = ");
+            int firstNumb = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число = ");
+            int secondNumb = Convert.ToInt32(Console.ReadLine());
+            if (firstNumb>secondNumb)
+            Console.WriteLine($"{firstNumb} больше {secondNumb}");
+            else
+            {
+                Console.WriteLine($"{secondNumb} больше {firstNumb}");
+            }
+        }
+    }
+}
