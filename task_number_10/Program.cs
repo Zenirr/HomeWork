3f1630ecdaf9c83c9db6785a35a7a5b8b8e324af
Zenirr <zenirr@mail.ru>
2022-08-24 23:49:58 +0600

    Added and solved task_number_10

---
+++ task_number_10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_number_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Впишите трёхзначное число:");
+            int a = int.Parse(Console.ReadLine());
+
+            if(100 <= a && a <= 999)
+            {
+            int hundreds = a/100;
+            int ones = a%10;
+            int res = (a - hundreds*100 - ones)/10;
+            Console.WriteLine($"Вторая цифра число {a} = {res}");
+            }
+            else
+            {
+                Console.WriteLine($"Число {a} не трёхзначное");
+            }
+        }
+    }
+}
