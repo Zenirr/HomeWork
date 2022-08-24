3df1a5ef94541b34a3937f13406c0736c1a47cbe
Zenirr <zenirr@mail.ru>
2022-08-25 00:13:21 +0600

    added and "solved" task_number_13

---
+++ task_number_13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task_number_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 10;
+            Console.WriteLine("Заданное число: {numberOne}");
+
+            Console.WriteLine($"{numberOne} - третьей цифры нет")
+        }
+    }
+}
