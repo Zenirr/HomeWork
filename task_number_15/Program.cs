3ef1d1a87947979dbdea27b85e07e1581bd6b66b
Zenirr <zenirr@mail.ru>
2022-08-25 07:36:44 +0600

    added and solved task_number_15

---
+++ task_number_15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_number_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите цифру которая обозначает день недели: ");
+            int day = int.Parse(Console.ReadLine());
+            
+            if((day==7)||(day==6))
+            {
+                Console.WriteLine("Это выходной");
+            }
+            else
+            {
+                Console.WriteLine("Либо это не выходной, либо такого дня недели не существует");
+            }
+
+        }
+    }
+}
