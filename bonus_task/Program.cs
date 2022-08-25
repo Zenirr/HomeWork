8216dfa6bf33de57cef6ef07d33046c1ca20e2d2
Zenirr <zenirr@mail.ru>
2022-08-25 07:53:36 +0600

    added and solved bonus_task

---
+++ bonus_task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace bonus_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if(((number%7)==0)&&((number%23)==0))
+            {
+                Console.WriteLine($"Число {number} кратно 7 и 23");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} не кратно 7 и 23");
+            }
+        }
+    }
+}
