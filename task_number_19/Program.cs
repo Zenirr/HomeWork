952d1f99d5f78c92201ad89a86617d0543ef0ed1
Zenirr <zenirr@mail.ru>
2022-08-31 21:12:33 +0600

    added and solved task_number_19

---
+++ task_number_19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_number_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите пятизначное число: ");
+            string strNumbers = Console.ReadLine();
+            int intNumbers = int.Parse(strNumbers);
+            if ((intNumbers>9999) && (intNumbers<100000))
+            {
+                if ((strNumbers[0]==strNumbers[4])&& (strNumbers[1]==strNumbers[3]))
+                Console.WriteLine("Число "+strNumbers+" полиндром.");
+                else
+                Console.WriteLine("Число "+strNumbers+" не полиндром");
+            }
+            else 
+            {
+                Console.WriteLine("Это не пятизначное число"); 
+            }
+        }
+    }
+}
