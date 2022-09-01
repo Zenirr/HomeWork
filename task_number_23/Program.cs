3f15a207929c1a5d35d7f702b73aac734fddcc8f
Zenirr <zenirr@mail.ru>
2022-09-01 17:13:15 +0600

    added and solved task_number_23

---
+++ task_number_23/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_number_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите цифру : ");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i<=number; i++)
+            {
+                Console.Write(" "+Math.Pow(i,3)+",");
+            }
+        }
+    }
+}
