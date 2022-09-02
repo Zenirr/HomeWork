be668f47821af09db23725dd8a1034b4ab63a054
Zenirr <zenirr@mail.ru>
2022-09-02 09:18:39 +0600

    added and solved task_number_27

---
+++ task_number_27/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task_number_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string numbers = Console.ReadLine();
+            int result = 0;
+
+            for(int i=0; i<=numbers.Length-1; i++)
+            {
+                result +=  numbers[i] - '0';
+            }
+            Console.WriteLine("Сумма чисел = " + result);
+        }
+    }
+}
