a44a763bfe13678f60135e52b420a9f0fa86997b
Zenirr <zenirr@mail.ru>
2022-09-02 08:19:01 +0600

    added and solved task_number_25

---
+++ task_number_25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_number_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            
+            Console.Write("Введите число которое нужно ввести в степень: ");
+            int justNumber = int.Parse(Console.ReadLine());
+            Console.Write("Введите степень: ");
+            int degreeNumber = int.Parse(Console.ReadLine());
+            int result = 1;
+
+            for(int i = 0;i<degreeNumber;i++)
+            {
+                result *= justNumber;
+
+            }
+            Console.WriteLine("Результата возведения в степень: "+ result);
+        }
+    }
+}
