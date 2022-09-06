4ed1fc4894ffbc2d69f0ed983e2f88c837e1bfb1
Zenirr <zenirr@mail.ru>
2022-09-06 23:31:45 +0600

    added and solved task_number_34

--- bonus_task/Program.cs
+++ bonus_task/Program.cs
@@ -6,35 +6,74 @@ namespace bonus_task
     {
         static void Main(string[] args)
         {
-            bool isTriangleExist(int sideA, int sideB, int sideC)
-            {
+            // bool isTriangleExist(int sideA, int sideB, int sideC)
+            // {
                 
-            if(((sideA+sideB)>sideC)&&((sideA+sideC)>sideB)&&((sideC+sideB)>sideA))
-            {
+            // if(((sideA+sideB)>sideC)&&((sideA+sideC)>sideB)&&((sideC+sideB)>sideA))
+            // {
                 
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-            }
+            //     return true;
+            // }
+            // else
+            // {
+            //     return false;
+            // }
+            // }
 
-            Console.Write("Введите первую сторону треугольника: ");
-            int sideA = int.Parse(Console.ReadLine());
-            Console.Write("Введите первую сторону треугольника: ");
-            int sideB = int.Parse(Console.ReadLine());
-            Console.Write("Введите первую сторону треугольника: ");
-            int sideC = int.Parse(Console.ReadLine());
+            // Console.Write("Введите первую сторону треугольника: ");
+            // int sideA = int.Parse(Console.ReadLine());
+            // Console.Write("Введите первую сторону треугольника: ");
+            // int sideB = int.Parse(Console.ReadLine());
+            // Console.Write("Введите первую сторону треугольника: ");
+            // int sideC = int.Parse(Console.ReadLine());
 
-            if(isTriangleExist(sideA,sideB,sideC))
-            {
-                Console.WriteLine("Triangle is exist");
-            }
-            else
-            {
-                Console.WriteLine("Triangle doesn't exist");
-            }
+            // if(isTriangleExist(sideA,sideB,sideC))
+            // {
+            //     Console.WriteLine("Triangle is exist");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Triangle doesn't exist");
+            // }
+        
+        // Задача 42: Напишите программу, которая будет преобразовывать
+        // десятичное число в двоичное.
+        // 45 -> 101101
+        // 3 -> 11
+        // 2 -> 10
+
+int[] DECtoBIN(int num)
+{
+    int[] result = new int[32];
+
+    for (int i = 0; num != 0; i++)
+    {
+        result[result.Length - 1 - i] = num % 2;
+        num = num / 2;
+    }
+
+    return result;
+}
+
+string DECtoBINforSTR(int num)
+{
+    string result = String.Empty;
+    
+    for (int i = 0; num != 0; i++)
+    {
+        result = Convert.ToString(num % 2) + result;
+        num = num / 2;
+    }
+
+    return result;
+}
+
+Console.WriteLine("Enter a integer number...");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"This number is convert to binary from number:\n{String.Join("", DECtoBIN(number))}");
+
+Console.WriteLine($"This number is convert to binary from string:\n{DECtoBINforSTR(number)}");
 
         }
     }
---
+++ task_number_34/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task_number_34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] mass = new int [4];
+            Random rand  = new Random();
+            int count = 0;
+
+            for (int i = 0; i<=3;i++)
+            {
+                mass [i] = rand.Next(100,1000);
+                Console.Write(mass[i]+" ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i<=3;i++)
+            {
+                if ((mass[i]%2) == 0)
+                {
+                    count++;
+                }
+            }
+            
+            Console.WriteLine("Число чётных чисел = "+ count);
+        }
+    }
+}
