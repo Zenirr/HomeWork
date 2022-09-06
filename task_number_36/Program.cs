30815f3ff961e86344e70d939e49989d20c22547
Zenirr <zenirr@mail.ru>
2022-09-07 03:08:38 +0600

    added and solved task_number_36

---
+++ task_number_36/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task_number_36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] mass = new int [4];
+            Random rand  = new Random();
+            int count = 0;
+
+            for (int i = 0; i<=3;i++)
+            {
+                mass [i] = rand.Next(-100,101);
+                Console.Write(mass[i]+" ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i<=3;i++)
+            {
+                if ((i%2)!=0)
+                count += mass[i];
+            }           
+            Console.Write("Сумма нечётных элементов массива = "+count);
+        }
+    }
+}
