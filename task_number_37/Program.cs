074220c7664cd2882524e79669974c49f5cbba54
Zenirr <zenirr@mail.ru>
2022-09-08 01:20:37 +0600

    added and solved task_number_37

---
+++ task_number_37/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task_number_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] mass = new int [4];
+            Random rand  = new Random();
+            int result = 0, i = 0, j=3;
+
+            for (int m = 0; m<=3;m++)
+            {
+                mass [m] = rand.Next(-100,101);
+                Console.Write(mass[m]+" ");
+            }
+            
+            Console.WriteLine();
+            while (i < j)
+            {
+                result = mass [i]*mass[j];
+                Console.Write($"{result} ");
+                i++;
+                j--;
+            }       
+        }
+    }
+}
