c26137576a573a032ffa74273dc043ecbc448e6b
Zenirr <zenirr@mail.ru>
2022-09-11 22:01:59 +0600

    added and solved task_number_41

---
+++ task_number_41/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task_number_41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество чисел которые вы хотите ввести: ");
+            int elementsCount = int.Parse(Console.ReadLine());
+            int [] array = new int [elementsCount];
+            int max = 0;
+
+            for(int i = 0;i<array.Length;i++)
+            {
+                Console.Write($"Введите число под номером {i+1}: ");
+                array [i] = int.Parse(Console.ReadLine());
+            }
+
+            max = array [0];
+
+            for(int i = 0;i<array.Length;i++)
+            {
+                if (max<array[i])
+                max = array[i];
+            }
+
+            Console.WriteLine($"Максимальное число в массиве = {max}"); 
+        }
+    }
+}
