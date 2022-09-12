0109beb7f695cc2b30e4a91c8e9c913939d9d57e
Zenirr <zenirr@mail.ru>
2022-09-12 21:49:46 +0600

    added and solved task_number_47

---
+++ task_number_47/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task_number_47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов: ");
+            int n = int.Parse(Console.ReadLine());
+            double [,] matrix = new double [m,n];
+            Random rand = new Random();
+
+            void FillAndPrintMatrix(double [,] array)
+            {
+            for (int i = 0; i<m ;i++)
+            {
+                for (int j = 0; j<n ;j++)
+                {
+                    matrix [i,j] = Math.Round(rand.Next(1,10)+rand.NextDouble(),2);
+                    Console.Write(matrix [i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            }
+
+            FillAndPrintMatrix(matrix);
+
+        }
+    }
+}
