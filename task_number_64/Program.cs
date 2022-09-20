0fd62ce5c709b5e803b4f9fda2ac52fd8909cea0
Zenirr <zenirr@mail.ru>
2022-09-21 03:12:18 +0600

    added and solved task_number_64

---
+++ task_number_64/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_number_64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("Введите M ");
+        int startNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите N ");
+        int maxNumber =  Convert.ToInt32(Console.ReadLine());;
+        PrintNumber(startNumber, maxNumber);
+
+        void PrintNumber(int min, int count)
+        {
+            if (count < min) return;
+            PrintNumber(min, count-1);
+            Console.Write(count + ", ");
+        }
+        
+        }
+    }
+}
